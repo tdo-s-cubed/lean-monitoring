38dde6c075b190f21b43e0cb4b4b25183f5939b0
tdo-s-cubed <57481859+tdo-s-cubed@users.noreply.github.com>
2019-12-16 11:20:14 +0100

    removed import line drawer component

--- src/components/Drawer-header.js
+++ src/components/Drawer-header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink, Link } from "react-router-dom";
+// import { NavLink, Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
